5b4df40c39668b88a5dfe6b60608aedf4056fd94
Ekinsley2002 <146798945+Ekinsley2002@users.noreply.github.com>
2023-10-02 21:51:15 -0700

    Create styles.cs

---
+++ cs212/homework/3/styles.cs
@@ -0,0 +1,10 @@
+/* styles.css */
+@font-face {
+  font-family: 'Lato Hairline';
+  src: url('Lato-Thin.ttf') format('truetype');
+}
+
+.custom-font {
+  font-family: 'Lato Hairline', Helvatica; /* Use your custom font */
+  font-size: 50px;
+}
